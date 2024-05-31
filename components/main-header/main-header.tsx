16015c759ee9f89299ad9d29e69914da78b7df6f
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2024-05-31 12:22:05 +0530

    Add custom component for the header background

---
+++ components/main-header/main-header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import MainHeaderBackground from "./main-header-background";
+import logoImg from "@/assets/logo.png";
+import styles from "./main-header.module.css";
+
+const MainHeader = () => {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          Next Level Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default MainHeader;
