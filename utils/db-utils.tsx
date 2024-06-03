5840da8b3f84416f31c20bac31f4e32568937a8c
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2024-06-03 15:12:08 +0530

    Add error component to handle errors from the database

--- utils/db-utils.tsx
+++ utils/db-utils.tsx
@@ -6,6 +6,7 @@ const db = sql("meals.db");
 
 const getMeals = async () => {
   await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+  // throw Error();
   return db.prepare("SELECT * FROM meals").all() as Array<Meal>;
 };
 
