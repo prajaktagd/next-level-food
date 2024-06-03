f6ce6f653598e72ca6a075c03f5dae4e60767bd8
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2024-06-03 15:55:22 +0530

    Add the not found component

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>404 Not Found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
